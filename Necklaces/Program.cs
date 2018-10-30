f178225729ca7a30451c6d29ea5c6cb22898f80b
newLana <newLana@users.noreply.github.com>
2018-10-30 11:55:35 +0200

    Add project files.

---
+++ Necklaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Necklaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] beads = new int[] { 7, 2, 4, 0, 5, 6, 1, 3 };
+            foreach (var bead in beads)
+            {
+                Console.Write(bead + " ");
+            }
+
+            Console.WriteLine($"\nResult: {GetLargestNecklace(beads)}");
+            Console.ReadKey();
+        }
+
+        static int GetLargestNecklace(int[] b)
+        {
+            string necklaces = "!|";
+            int count = 0;
+            for (int i = 0; i < b.Length; i++)
+            {
+                int current = i;
+                while (!necklaces.Contains("|" + current + "|"))
+                {
+                    necklaces += current + "|";
+                    current = b[current];
+                }
+                count = Math.Max(count, necklaces.Substring(necklaces.LastIndexOf("!") + 1).Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries).Length);
+                necklaces += "!|"; 
+            }
+            return count;
+        }
+    }
+}
